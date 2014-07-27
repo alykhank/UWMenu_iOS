797d6958bb88e1892863f1e27fc8d651a61d050a
Alykhan Kanji <github@alykhan.com>
2014-07-27 15:38:24 -0700

    Change master and detail, storyboard

--- UW Menu/DetailViewController.m
+++ UW Menu/DetailViewController.m
@@ -8,15 +8,29 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
-@property (nonatomic, strong) NSMutableArray *restaurant;
+@interface DetailViewController () {
+    NSMutableArray *_restaurant;
+    NSMutableDictionary *_meals;
+}
 - (void)configureView;
 - (void)organizeMenuContents;
+
+typedef enum MEAL {
+    MONDAY_LUNCH,
+    MONDAY_DINNER,
+    TUESDAY_LUNCH,
+    TUESDAY_DINNER,
+    WEDNESDAY_LUNCH,
+    WEDNESDAY_DINNER,
+    THURSDAY_LUNCH,
+    THURSDAY_DINNER,
+    FRIDAY_LUNCH,
+    FRIDAY_DINNER
+} mealName;
+
 @end
 
 @implementation DetailViewController
-
-@synthesize restaurant = _restaurant;
 
 #pragma mark - Managing the detail item
 
@@ -24,7 +38,9 @@
 {
     if (_detailItem != newDetailItem) {
         _detailItem = newDetailItem;
-        
+
+        // Flatten meals into an array.
+        [self organizeMenuContents];
         // Update the view.
         [self configureView];
     }
@@ -33,9 +49,8 @@
 - (void)configureView
 {
     // Update the user interface for the detail item.
-
     if (self.detailItem) {
-        self.navigationItem.title = [self.detailItem objectForKey:@"Name"];
+        self.navigationItem.title = [self.detailItem objectForKey:@"outlet_name"];
     }
 }
 
@@ -45,7 +60,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.detailDescriptionTableView.delegate = self;
     self.detailDescriptionTableView.dataSource = self;
-    [self organizeMenuContents];
     [self configureView];
 }
 
@@ -57,65 +71,102 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 5;
+    int sections = 0;
+    for (NSDictionary *day in [self.detailItem objectForKey:@"menu"]) {
+        if ([[day objectForKey:@"meals"] objectForKey:@"lunch"]) {
+            sections += 1;
+        }
+        if ([[day objectForKey:@"meals"] objectForKey:@"dinner"]) {
+            sections += 1;
+        }
+    }
+    return sections;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    NSString *title;
-    switch (section) {
-        case 0:
-            title = @"Monday";
-            break;
-        case 1:
-            title = @"Tuesday";
-            break;
-        case 2:
-            title = @"Wednesday";
-            break;
-        case 3:
-            title = @"Thursday";
-            break;
-        case 4:
-            title = @"Friday";
-            break;
-        default:
-            break;
-    }
-    return title;
+    return [[_meals objectForKey:[NSNumber numberWithInteger:section]] objectForKey:@"mealName"];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return ((NSDictionary *) self.restaurant[section]).count;
+    if ([_meals objectForKey:[NSNumber numberWithInteger:section]]) {
+        return [[[_meals objectForKey:[NSNumber numberWithInteger:section]] objectForKey:@"mealData"] count];
+    } else {
+        return 0;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Meal" forIndexPath:indexPath];
-    
-    NSDictionary *object;
-    if (indexPath.row == 0) {
-        object = [self.restaurant[indexPath.section] objectForKey:@"Lunch"];
-        cell.textLabel.text = @"Lunch";
+    NSArray *mealData = [[_meals objectForKey:[NSNumber numberWithInteger:indexPath.section]] objectForKey:@"mealData"];
+    if ([mealData[indexPath.row] objectForKey:@"product_name"]) {
+        cell.textLabel.text = [mealData[indexPath.row] objectForKey:@"product_name"];
     }
-    else {
-        object = [self.restaurant[indexPath.section] objectForKey:@"Dinner"];
-        cell.textLabel.text = @"Dinner";
+    NSString *dietType = [mealData[indexPath.row] objectForKey:@"diet_type"];
+    if (dietType && ![dietType isKindOfClass:[NSNull class]]) {
+        cell.detailTextLabel.text = dietType;
+    } else {
+        cell.detailTextLabel.text = nil;
     }
-    cell.detailTextLabel.text = [[object objectForKey:@"Items"] objectForKey:@"result"];
     return cell;
+}
+
+- (NSNumber *)computeDayValueWith:(NSString *)dayName andIsLunchMealType:(BOOL)isLunch
+{
+    NSNumber *mealNumber = nil;
+    if (isLunch) {
+        if ([dayName isEqualToString:@"Monday"]) {
+            mealNumber = [NSNumber numberWithInt:MONDAY_LUNCH];
+        } else if ([dayName isEqualToString:@"Tuesday"]) {
+            mealNumber = [NSNumber numberWithInt:TUESDAY_LUNCH];
+        } else if ([dayName isEqualToString:@"Wednesday"]) {
+            mealNumber = [NSNumber numberWithInt:WEDNESDAY_LUNCH];
+        } else if ([dayName isEqualToString:@"Thursday"]) {
+            mealNumber = [NSNumber numberWithInt:THURSDAY_LUNCH];
+        } else if ([dayName isEqualToString:@"Friday"]) {
+            mealNumber = [NSNumber numberWithInt:FRIDAY_LUNCH];
+        }
+    } else {
+        if ([dayName isEqualToString:@"Monday"]) {
+            mealNumber = [NSNumber numberWithInt:MONDAY_DINNER];
+        } else if ([dayName isEqualToString:@"Tuesday"]) {
+            mealNumber = [NSNumber numberWithInt:TUESDAY_DINNER];
+        } else if ([dayName isEqualToString:@"Wednesday"]) {
+            mealNumber = [NSNumber numberWithInt:WEDNESDAY_DINNER];
+        } else if ([dayName isEqualToString:@"Thursday"]) {
+            mealNumber = [NSNumber numberWithInt:THURSDAY_DINNER];
+        } else if ([dayName isEqualToString:@"Friday"]) {
+            mealNumber = [NSNumber numberWithInt:FRIDAY_DINNER];
+        }
+    }
+    return mealNumber;
 }
 
 - (void)organizeMenuContents
 {
-    if (self.detailItem) {
-        self.restaurant = [NSMutableArray arrayWithCapacity:5];
-        self.restaurant[0] = [[self.detailItem objectForKey:@"Menu"] objectForKey:@"Monday"];
-        self.restaurant[1] = [[self.detailItem objectForKey:@"Menu"] objectForKey:@"Tuesday"];
-        self.restaurant[2] = [[self.detailItem objectForKey:@"Menu"] objectForKey:@"Wednesday"];
-        self.restaurant[3] = [[self.detailItem objectForKey:@"Menu"] objectForKey:@"Thursday"];
-        self.restaurant[4] = [[self.detailItem objectForKey:@"Menu"] objectForKey:@"Friday"];
+    _meals = [NSMutableDictionary dictionary];
+    for (NSDictionary *day in [self.detailItem objectForKey:@"menu"]) {
+        NSString *dayName = [day objectForKey:@"day"];
+        NSArray *lunch = [[day objectForKey:@"meals"] objectForKey:@"lunch"];
+        NSArray *dinner = [[day objectForKey:@"meals"] objectForKey:@"dinner"];
+        if ([lunch count]) {
+            NSMutableDictionary *mealValue = [NSMutableDictionary dictionary];
+            [mealValue setValue:[NSString stringWithFormat:@"%@ Lunch", dayName] forKey:@"mealName"];
+            [mealValue setValue:lunch forKey:@"mealData"];
+            NSNumber *mealNumber;
+            mealNumber = [self computeDayValueWith:dayName andIsLunchMealType:TRUE];
+            [_meals setObject:mealValue forKey:mealNumber];
+        }
+        if ([dinner count]) {
+            NSMutableDictionary *mealValue = [NSMutableDictionary dictionary];
+            [mealValue setValue:[NSString stringWithFormat:@"%@ Dinner", dayName] forKey:@"mealName"];
+            [mealValue setValue:dinner forKey:@"mealData"];
+            NSNumber *mealNumber;
+            mealNumber = [self computeDayValueWith:dayName andIsLunchMealType:FALSE];
+            [_meals setObject:mealValue forKey:mealNumber];
+        }
     }
 }
 
